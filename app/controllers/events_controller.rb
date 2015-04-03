156408014bf0deae015bc049c50505acd46080dd
farishkash <Farish@Farishs-iMac.local>
2015-04-02 19:40:45 -0700

    Added Date and Time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@ class EventsController < ApplicationController
 	
 	def index
 		@events = Event.all
+
+		
 			@hash = Gmaps4rails.build_markers(@events) do |event, marker|
       		marker.lat event.latitude
       		marker.lng event.longitude
