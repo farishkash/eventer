dd58f0393e3fa5e4b25b2f47fd5d36a1a7f89fc9
farishkash <Farish@Farishs-iMac.local>
2015-04-07 18:34:21 -0700

    Heroku configuration

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,6 @@
 //= require turbolinks
 //= require underscore
 //= require gmaps/google
+//= require gmaps4rails/gmaps4rails.base
+//= require gmaps4rails/gmaps4rails.googlemaps
 //= require_tree .
